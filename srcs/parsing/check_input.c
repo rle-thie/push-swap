091452282a16c2f1201c0a1a6afc7ee8bafa91eb
rle-thie <robinlethiec1@gmail.com>
2022-03-01 15:49:20 +0100

    saveuu

--- srcs/parsing/check_input.c
+++ srcs/parsing/check_input.c
@@ -6,19 +6,61 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 13:58:23 by rle-thie          #+#    #+#             */
-/*   Updated: 2022/03/01 12:22:31 by rle-thie         ###   ########.fr       */
+/*   Updated: 2022/03/01 15:48:27 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
+void	ft_free(char **strs)
+{
+	int	i;
+
+	i = 0;
+	while (strs[i])
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+}
+
 int	ft_error(void)
 {
 	write(1, "Error\n", 6);
+	return (0);
+}
+
+char	**create_tab(int c, int ac, char **av)
+{
+	char **tab;
+	char **buff;
+	int	i;
+	
+	tab = malloc(sizeof(char *) * c + 1);
+	if (!tab)
+		return (NULL);
+	tab[c+1] = NULL;
+	i = 1;
+	while (i < ac)
+	{
+		if (ft_strchr(av[i], ' '))
+		{
+			y = 0;
+			buff = ft_split(av[i], ' ');
+			while ()
+			printf("ouais");
+		}
+		else
+			tab[i] = ft_strdup(av[i]);
+		i++;
+	}
+	free(tab);
+	// return (tab);
 	return (NULL);
 }
 
-int	ft_isnbr(char *str)
+int	ft_valid_arg(char *str)
 {
 	size_t	i;
 	
@@ -27,7 +69,9 @@ int	ft_isnbr(char *str)
 		i = 0;
 		while (i < ft_strlen(str))
 		{
-			if (!ft_isdigit(str[i]))
+			if ((i == 0 && str[i] == '+') || (i == 0 && str[i] == '-') || str[i] == ' ')
+				i++;
+			if (!ft_isdigit(str[i]) || (i != 0 && str[i] == '-') || (i != 0 && str[i] == '+'))
 				return (0);
 			i++;
 		}
@@ -38,21 +82,54 @@ int	ft_isnbr(char *str)
 
 // int	cmp_nbr(char *str)
 
+int	check_space_value(int ac, char **av)
+{
+	int	i;
+	char	**str;
+	int	c;
+	int y;
+
+	i = 1;
+	c = 0;
+	while (i < ac)
+	{
+		if (ft_strchr(av[i], ' '))
+		{
+			y = 0;
+			str = ft_split(av[i], ' ');
+			while (!str[i])
+				y++;
+			c = c + y;
+			ft_free(str);
+		}
+	}
+	return (c);
+}
+
 char **split_input(int ac, char **av)
 {
 	int i;
 	int	c;
+	char **tab;
 
 	c = 0;
 	i = 1;
 	while (i < ac)
 	{
-		if (ft_isnbr(av[i]) == 1)
+		if (ft_valid_arg(av[i]) == 1)
 			c++;
 		else
-			return(ft_error());
+		{
+			ft_error();
+			return(NULL);
+		}
 		i++;
 	}
+	c = c + check_space_value(ac, av);
+	tab = create_tab(c, ac, av);
+	i = 1;
+	// while (i < ac)
+	// 	printf("%s\n", tab[i++]);
 	printf("c=%d", c);
 	return(NULL);
 }
