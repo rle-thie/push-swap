b43119e8ba734ac981bc7bdfc029b1d992ea0b10
rle-thie <robinlethiec1@gmail.com>
2022-03-01 12:23:07 +0100

    graille

--- srcs/parsing/check_input.c
+++ srcs/parsing/check_input.c
@@ -6,22 +6,57 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 13:58:23 by rle-thie          #+#    #+#             */
-/*   Updated: 2022/02/28 18:36:29 by rle-thie         ###   ########.fr       */
+/*   Updated: 2022/03/01 12:22:31 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-char **split_input(int ac, char **av)
-{
-	
-}
-
 int	ft_error(void)
 {
 	write(1, "Error\n", 6);
-	return (0);
+	return (NULL);
 }
+
+int	ft_isnbr(char *str)
+{
+	size_t	i;
+	
+	if (str[0] != '\0')
+	{
+		i = 0;
+		while (i < ft_strlen(str))
+		{
+			if (!ft_isdigit(str[i]))
+				return (0);
+			i++;
+		}
+		return (1);
+	}
+	return (0);	
+}
+
+// int	cmp_nbr(char *str)
+
+char **split_input(int ac, char **av)
+{
+	int i;
+	int	c;
+
+	c = 0;
+	i = 1;
+	while (i < ac)
+	{
+		if (ft_isnbr(av[i]) == 1)
+			c++;
+		else
+			return(ft_error());
+		i++;
+	}
+	printf("c=%d", c);
+	return(NULL);
+}
+
 
 int check_input(int ac, char **av)
 {
